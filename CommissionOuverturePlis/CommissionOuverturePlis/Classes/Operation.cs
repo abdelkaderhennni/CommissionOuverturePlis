13908cd74cc90e6c639b1064cabe36f75b6b2985
Abdelkader HENNI <abdelkaderhenni2003@gmail.com>
2025-05-04 22:06:31 +0100

    Fixing errors and adding operations page

---
+++ CommissionOuverturePlis/CommissionOuverturePlis/Classes/Operation.cs
@@ -0,0 +1,10 @@
+﻿namespace CommissionOuverturePlis.Classes
+{
+    public class Operation
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+        public string Nature { get; set; }
+    }
+}
